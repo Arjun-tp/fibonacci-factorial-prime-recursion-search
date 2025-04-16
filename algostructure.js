21a73e9a785994c23b226df344658408564219f1
Arjun TP <arjuntpnambiar@gmail.com>
2025-04-16 13:50:34 -0400

    Added binary search

--- algostructure.js
+++ algostructure.js
@@ -89,3 +89,27 @@ console.log(linear);
 
 
 
+// Binary Search
+
+var binarySearch = function (arr, target) {
+console.log('arr: ', arr, " target: ", target)
+let start = 0;
+let end = arr.length - 1
+
+while (start <= end) {
+    let mid = Math.ceil((start + end) / 2)
+    if (target === arr[mid]) {
+        return mid
+    }
+    if (target <= arr[mid]) {
+        end = mid - 1
+    }
+    else {
+        start = mid + 1
+    }
+}
+return -1
+
+};
+
+
